e9e56b900b21ddf6d9a5c76fab949eb24f91001b
justinkhchan <justinkhchan@gmail.com>
2014-03-21 07:02:07 -0700

    First commit

---
+++ app/database/migrations/2014_03_21_022208_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::create('users', function($table) {
+		$table->increments('id');
+		$table->string('username', 64);
+		$table->string('password', 64);
+		$table->string('name', 128);
+		$table->timestamps();
+		});
+
+		DB::table('users')->insert(array(
+		'username' => 'admin',
+		'password' => Hash::make('password'),
+		'name' => 'Admin'
+		));
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('users');
+	}
+
+}
